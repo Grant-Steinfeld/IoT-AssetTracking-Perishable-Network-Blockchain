bc9a45286e7b3f34dc2ef60b7919a15082a3aed9
Grant Steinfeld <Grant.Steinfeld.Tech@gmail.com>
2019-09-06 16:33:46 -0400

    webhooks and mqtt explorations

---
+++ ParticleElectron-http/WatsonIoTAssetTracker.ino
@@ -0,0 +1,65 @@
+#include <Adafruit_DHT_Particle.h>
+#include <AssetTracker.h>
+#define DHTTYPE DHT11
+#define DHTPIN D3
+DHT dht(DHTPIN, DHTTYPE);
+AssetTracker t = AssetTracker();
+
+int ledonboard = D7;
+
+          
+
+void setup() {
+     
+   t.begin();  // Start the DHT sensor
+   Serial.begin(9600); 
+       
+         
+  // Subscribe to the integration response event
+  Particle.subscribe("hook-response/temperature", myHandler, MY_DEVICES);
+  Particle.function("GetCurrTemp",AssetTrackerGetCurrentTemp);
+   
+   dht.begin();
+   // LED pin configuration
+   pinMode(ledonboard, OUTPUT);
+   // Turn off LED on the Particle board when the application starts
+   digitalWrite(ledonboard, LOW);
+
+
+}   
+
+void myHandler(const char *event, const char *data) {
+   // Handle the integration response
+     Serial.println(data);
+}
+
+int AssetTrackerGetCurrentTemp(String Coordinates) {
+    float celsius, fahrenheit;
+    // Read Celsius and Fahrenheit values from DHT sensor
+    celsius    = dht.getTempCelcius();
+    fahrenheit = dht.getTempFarenheit();
+    // now that we have the readings, we can publish them to the cloud
+    // store celsius and fahrenheit temp readings in "temperature" stringified JSON
+    String temperature = String::format("{\"Celsius\":%f,\"Fahrenheit\":%f}", celsius, fahrenheit );
+    String TempPlusLocation;
+
+    TempPlusLocation = String("{\"Temp\":"+temperature+"}"); 
+
+
+    Particle.publish("temperature", TempPlusLocation, PRIVATE); // publish to webhook
+    Serial.print( "Sending AssetTrackerTemperatureEvent : " );
+    Serial.print(TempPlusLocation);
+    return 9795;
+
+}
+
+
+void loop() {
+  // Get some data
+  int ret = AssetTrackerGetCurrentTemp("bahhumbug");
+  // Trigger the integration
+  //Particle.publish("temperature", data, PRIVATE);
+  // Wait 60 seconds
+  Serial.println(ret);
+  delay(60000);
+}
---
+++ mqtt-wiotp/mqtttest.ino
@@ -0,0 +1,82 @@
+#include <MQTT.h>
+char *IOT_CLIENT = "d:_org_id:device_type:AssetTracker001";
+char *IOT_HOST = "b0wmtp.messaging.internetofthings.ibmcloud.com";
+char *IOT_PASSWORD = "IoTBlockchain";
+char *IOT_PUBLISH = "iot-2/evt/temp/fmt/json";
+char *IOT_USERNAME = "use-token-auth";
+
+int count = 0;
+
+MQTT client( IOT_HOST, 1883, callback );
+
+void setup() {
+  Serial.begin( 9600 );
+
+  while( !Serial.available() ) {
+    Particle.process();
+  }
+
+  client.connect(
+    IOT_CLIENT,
+    IOT_USERNAME,
+    IOT_PASSWORD
+  );
+
+  if( client.isConnected() ) {
+    Serial.println( "Connected." );
+    // client.subscribe( IOT_SUBSCRIBE );
+  } else {
+    Serial.println( "Connection failed");
+  }
+}
+
+// DHT
+#include <Adafruit_DHT_Particle.h>
+
+// DHT Sensor parameters
+// sensor type: [DHT11, DHT22, DHT21, AM2301]
+#define DHTTYPE DHT11
+// which digital pin for the DHT
+#define DHTPIN D3
+// DHT Sensor global
+DHT dht(DHTPIN, DHTTYPE);
+
+String GetCurrentTemp() {
+    float celsius, fahrenheit;
+
+    // Read Celsius and Fahrenheit values from DHT sensor
+    celsius    = dht.getTempCelcius();
+    fahrenheit = dht.getTempFarenheit();
+
+    // now that we have the readings, we can publish them to the cloud
+    // store celsius and fahrenheit temp readings in "temperature" stringified JSON
+    String temperature = String::format("{\"Celsius\":%f,\"Fahrenheit\":%f}", celsius, fahrenheit );
+    String TempReading;
+    TempReading = String("{\"Temp\":"+temperature+"}");
+    Serial.print( "Sending MQTT TemperatureEvent : " );
+    Serial.println( TempReading );
+    return( TempReading );
+}
+
+
+void loop() {
+  count = count + 1;
+
+  // String JSON;
+  // count = AssetTrackerGetCurrentTemp( JSON );
+  // JSON = String::format("{\"temp\":%d}", count);
+  String JSON = GetCurrentTemp();
+  Serial.println( JSON );
+  client.publish( IOT_PUBLISH, JSON);
+
+  client.loop();
+  if( client.isConnected() ) {
+    Serial.println( "Connected." );
+  }
+
+  delay( 10000 );
+}
+
+void callback( char* topic, byte* payload, unsigned int length ) {
+
+}
